6cdc28fc245dcd302ee6c9fabd9b65a65c54e1ae
ymiandi <571785481@qq.com>
2022-07-19 19:00:17 +0800

    初始化

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,15 +4,15 @@ export default defineConfigWithTheme({
   base: '/lilly/',
   title: 'Gradyll',
   description: 'This is b vite-ts-blog.',
-  lastUpdated: true,
+  // lastUpdated: true,
   lang: 'en-US',
   themeConfig: {
     // outlineTitle: 'In hac pagina',
     siteTitle: 'Gradyll',
     logo: '/logo.png',
     nav: [
-      { text: 'Guide', activeMatch: `^/guide/`, link: '/guide/index', },
-      { text: 'JavaScript', activeMatch: `^/javaScript/`, link: '/javaScript/index' },
+      // { text: 'Guide', activeMatch: `^/guide/`, link: '/guide/index', },
+      { text: '生活', activeMatch: `^/life/`, link: '/life/eat' },
 
     ],
     sidebar: {
@@ -24,11 +24,12 @@ export default defineConfigWithTheme({
           ]
         }
       ],
-      '/javaScript/': [
+      '/life/': [
         {
-          text: 'javaScript',
+          text: '生活',
           items: [
-            { text: 'js', link: '/javaScript/index' }
+            { text: '吃', link: '/life/eat/index' },
+            { text: '喝', link: '/life/drink/index' },
           ]
         }
       ]
@@ -36,10 +37,10 @@ export default defineConfigWithTheme({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/' },
     ],
-    editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
-      text: 'Edit this page on GitHub'
-    },
+    // editLink: {
+    //   pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+    //   text: 'Edit this page on GitHub'
+    // },
     footer: {
       message: 'Released under the MIT License.',
       copyright: `Copyright © 2022-${new Date().getFullYear()} Gradyll`
