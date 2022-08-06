4939766b9e092868792fdd38654b1481c114ddd7
ymiandi <571785481@qq.com>
2022-08-06 14:58:10 +0800

    主页功能修改

--- .vitepress/theme/components/Layout.vue
+++ .vitepress/theme/components/Layout.vue
@@ -1,36 +1,57 @@
 <!-- .vitepress/theme/Layout.vue -->
 <template>
   <div class="ll-wrapper">
-    <span class="ll-title">
-      <em class="ll-des">我们在一起</em>
+    <div class="ll-title">
+      <em class="ll-des">在一起</em>
       <span class="ll-day-num">{{ Time }}</span>
-    </span>
+      
+    </div>
+    <div class="ll-time"></div>
   </div>
 </template>
-<script setup lang="ts">
+<script setup>
 import { onMounted, ref } from "vue";
 let Time = ref(0);
 
-const setAnniversary = (time: String): number => {
-  let date: number = new Date(time as string).getTime();
-  let cur: number = Date.now();
-  let days: number = Math.floor((cur - date) / 1000 / 60 / 60 / 24);
+const setAnniversary = (time) => {
+  let date= new Date(time).getTime();
+  let cur = Date.now();
+  let days = Math.floor((cur - date) / 1000 / 60 / 60 / 24);
   return days;
 };
 
+const getTime = () => {
+  const date = new Date()
+  const year = date.getFullYear()
+  const h = date.getHours()
+  const m = date.getMinutes() < 10 ? '0'+ date.getMinutes() : date.getMinutes()
+  const s = date.getSeconds() < 10 ? '0'+ date.getSeconds() : date.getSeconds()
+  return `${h}:${m}:${s}`
+}
+
+const mountedDom = () => {
+  document.querySelector('.ll-time').innerHTML = getTime()
+}
+
+
 onMounted(() => {
   Time.value = setAnniversary("2021-10-30");
+  setInterval(() => {
+    mountedDom()
+  }, 1000)
 });
 </script>
 <style scoped lang="less">
 .ll-wrapper {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: calc(100vh - 310px);
   font-size: 36px;
 
   .ll-title {
+    margin-bottom: 50px;
   }
   .ll-des:not(.dark) {
     background: linear-gradient(315deg, #42d392 25%, #647eff);
--- src/life/eat/index.md
+++ src/life/eat/index.md
@@ -1,28 +1,25 @@
 # 这里是美食
 
 ## 宁波
-### 一、大饼豆瓣酱
+### 一、 大饼豆瓣酱 
 - **店名**：安徽永其小吃店
 - **地点**：宁波市江厦街道开明街249弄28-1
 - **营业时间**：10:30-20:30
+- **是否去过**：<input type="checkbox"  disabled>
 
 <ImgShow :imgList="[
-  {
-    src: './img/shop-1/shop-1.jpg',
-  },
-  {
-    src: './img/shop-1/shop-2.jpg',
-  },
   {
     src: 'https://ci.xiaohongshu.com/befa0de6-9a0a-4052-8ac6-e5266b80a023?imageView2/2/w/1080/format/jpg',
   }
 ]"></ImgShow>
 
-### 二、犟山城烧鸡公
-- **店名**：犟山城烧鸡公
+### 二、犟山城烧鸡公 
+- **店名**：犟山城烧鸡公 
 - **地点**：天一广场258号
 - **营业时间**：不详
+- **是否去过**：<input type="checkbox"  disabled>
 
+  
 <ImgShow :imgList="[
   {
     src: 'https://ci.xiaohongshu.com/cf0d708b-1517-21e1-3b4e-d768c0b88fed?imageView2/2/w/1080/format/jpg',
@@ -42,6 +39,7 @@
 - **店名**：芝士肉饼x五花肉包菜卷
 - **地点**：台东长兴路北（小木屋）
 - **营业时间**：11点开门
+- **是否去过**：<input type="checkbox"  disabled>
 
 <ImgShow :imgList="[
   {
